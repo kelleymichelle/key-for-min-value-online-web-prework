cf0dc69d091e63c372b41b18e63a8c75390be266
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-25 20:57:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,17 @@
 require 'pry'
 def key_for_min_value(name_hash)
  #binding.pry
-  min_key = nil
-  min_value = nil
+  min_k = nil
+  min_v = nil
   if name_hash.empty?
     return nil
   else
-    name_hash.each do |key, value|
-        if value < min_value
-          min_value = value
-          min_key = key
+    name_hash.each do |k, v|
+        if v < min_v
+          min_v = v
+          min_k = k
        end  
      end
     end 
-  return min_key
+  return min_k
 end
