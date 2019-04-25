0cf4744bc4c3e5c0eb2f7948ee2ca8fb37c9de52
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-25 20:26:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
     return nil
   else
     #min_key = [ ]
-    min_value = 0
+    min_value = nil
       name_hash.each do |key, value|
         if value < min_value
           min_value = value
